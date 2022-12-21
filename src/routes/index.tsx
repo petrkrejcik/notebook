3f4a00d0cbe32a93d24bb33b0cd76ce4762bda7f
Petr Krejcik <p.krejcik@stuart.com>
2022-12-21 23:02:45 +0100

    initial commit

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,18 +1,9 @@
-import Counter from "~/components/Counter";
 import "./index.css";
 
 export default function Home() {
   return (
     <main>
-      <h1>Hello world!</h1>
-      <Counter />
-      <p>
-        Visit{" "}
-        <a href="https://start.solidjs.com" target="_blank">
-          start.solidjs.com
-        </a>{" "}
-        to learn how to build Solid apps.
-      </p>
+      <textarea></textarea>
     </main>
   );
 }
