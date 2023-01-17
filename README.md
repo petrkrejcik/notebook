05ff07685577edb68852e00465a88030290488fe
Petr Krejcik <p.krejcik@stuart.com>
2023-01-17 23:12:10 +0100

    editor shown

--- README.md
+++ README.md
@@ -6,9 +6,6 @@ A simple notebook app.
 
 ```bash
 yarn dev
-
-# or start the server and open the app in a new browser tab
-npm run dev -- --open
 ```
 
 ## Building
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,9 +1,32 @@
 import "./index.css";
+import { onMount } from "solid-js";
+import type {editor as editorType} from "monaco-editor/esm/vs/editor/editor.api";
 
 export default function Home() {
+  let parent: HTMLDivElement;
+  let editor: editorType.IStandaloneCodeEditor;
+
+  const setupEditor = (mEditor: typeof editorType) => {
+    editor = mEditor.create(parent, {
+      minimap: { enabled: false },
+      lineNumbers: "off",
+      theme: 'vs-dark',
+      renderLineHighlight: 'none',
+      value: 'console.log("Hello, world!");',
+      language: 'javascript',
+      padding: { top: 12 },
+    });
+  };
+  
+  onMount(async () => {
+    const { editor: mEditor, languages } = await import("monaco-editor/esm/vs/editor/editor.api");
+    // languages.register({ id: 'markdown' });
+    setupEditor(mEditor);
+  });
+
   return (
     <main>
-      <textarea></textarea>
+      <div style={{ height: "100%", width: "100%" }} ref={parent}></div>
     </main>
   );
 }
